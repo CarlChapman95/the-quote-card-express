4f23bf6ca2245ea43a28368d5cd5ff97b114a638
CarlChapman95 <crabb.chap@gmail.com>
2025-06-29 14:45:24 -0400

    Commit 5

--- public/js/index.js
+++ public/js/index.js
@@ -41,7 +41,7 @@ function loopThroughQuotes() {
 setTimeout(loopThroughQuotes, 3000); */
 
 async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
+    const client_id = "";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
         const response = await fetch(endpoint);
